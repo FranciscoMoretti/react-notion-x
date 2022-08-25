bd642ee9438718984017ab55f169c608740fbf89
Francisco Moretti <franciscoemoretti@gmail.com>
2022-08-25 14:10:54 +0100

    refine checkbox to match notion style

--- packages/react-notion-x/src/components/checkbox.tsx
+++ packages/react-notion-x/src/components/checkbox.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import CheckIcon from '../icons/check'
+import { SvgCheckIcon, SvgCheckBoxIcon } from '../icons/check'
 import '../styles.css'
 
 export const Checkbox: React.FC<{
@@ -11,15 +11,17 @@ export const Checkbox: React.FC<{
 
   if (isChecked) {
     content = (
-      <div className='notion-property-checkbox-checked'>
-        <CheckIcon />
+      <div className='notion-property-checkbox notion-property-checkbox-checked'>
+        <SvgCheckIcon />
       </div>
     )
   } else {
-    content = <div className='notion-property-checkbox-unchecked' />
+    content = (
+      <div className='notion-property-checkbox'>
+        <SvgCheckBoxIcon />
+      </div>
+    )
   }
 
-  return (
-    <span className='notion-property notion-property-checkbox'>{content}</span>
-  )
+  return <span className='notion-property'>{content}</span>
 }
