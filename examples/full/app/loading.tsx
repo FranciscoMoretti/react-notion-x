81138f55049213cfd701ea23fe734773efdf17bd
Francisco Moretti <franciscoemoretti@gmail.com>
2023-02-05 17:19:40 -0300

    loading in loading page

---
+++ examples/full/app/loading.tsx
@@ -0,0 +1,6 @@
+import { LoadingStyled } from '../components/LoadingStyled'
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <LoadingStyled />
+}
--- examples/full/components/LoadingStyled.tsx
+++ examples/full/components/LoadingStyled.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { LoadingIcon } from './LoadingIcon'
 import styles from './styles.module.css'
 
-export const Loading: React.FC = () => (
+export const LoadingStyled: React.FC = () => (
   <div className={styles.container}>
     <LoadingIcon />
   </div>
