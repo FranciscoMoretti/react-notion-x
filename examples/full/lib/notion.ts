f51fee11297f92b6c59eff4ca5ec722825bfaae5
Francisco Moretti <franciscoemoretti@gmail.com>
2023-02-08 20:16:55 -0300

    del preview images. Error with the lib on webpack

--- examples/full/lib/notion.ts
+++ examples/full/lib/notion.ts
@@ -3,8 +3,9 @@ import { NotionAPI } from 'notion-client'
 import { NotionCompatAPI } from 'notion-compat'
 import { ExtendedRecordMap, SearchParams, SearchResults } from 'notion-types'
 
-import { previewImagesEnabled, useOfficialNotionAPI } from './config'
-import { getPreviewImageMap } from './preview-images'
+import { useOfficialNotionAPI } from './config'
+// import { previewImagesEnabled, useOfficialNotionAPI } from './config'
+// import { getPreviewImageMap } from './preview-images'
 
 const notion = useOfficialNotionAPI
   ? new NotionCompatAPI(new Client({ auth: process.env.NOTION_TOKEN }))
@@ -19,10 +20,10 @@ if (useOfficialNotionAPI) {
 export async function getPage(pageId: string): Promise<ExtendedRecordMap> {
   const recordMap = await notion.getPage(pageId)
 
-  if (previewImagesEnabled) {
-    const previewImageMap = await getPreviewImageMap(recordMap)
-    ;(recordMap as any).preview_images = previewImageMap
-  }
+  // if (previewImagesEnabled) {
+  //   const previewImageMap = await getPreviewImageMap(recordMap)
+  //   ;(recordMap as any).preview_images = previewImageMap
+  // }
 
   return recordMap
 }
