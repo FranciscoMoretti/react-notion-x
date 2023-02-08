fad554e41c133bc86fcba8bf906866bd627110a4
Francisco Moretti <franciscoemoretti@gmail.com>
2023-02-08 20:24:05 -0300

    generate static params

--- examples/full/app/[pageId]/page.tsx
+++ examples/full/app/[pageId]/page.tsx
@@ -1,19 +1,65 @@
 import { NotionAPI } from 'notion-client'
-import {
-  rootNotionPageId,
-  previewImagesEnabled,
-  rootDomain
-} from '../../lib/config'
+import { getAllPagesInSpace } from 'notion-utils'
 import { NotionPage } from '../../components/NotionPage'
+import {
+  isDev,
+  previewImagesEnabled,
+  rootDomain,
+  rootNotionPageId,
+  rootNotionSpaceId
+} from '../../lib/config'
+import * as notion from '../../lib/notion'
+// import { defaultMapPageUrl } from 'react-notion-x' // Not working from import
 
 export const revalidate = false // Do not revalidate
 
-const notion = new NotionAPI()
+const notionAPIInstance = new NotionAPI()
 
 async function getNotionPage(id: string) {
-  const recordMap = await notion.getPage(id)
+  const recordMap = await notionAPIInstance.getPage(id)
   return recordMap
   //     revalidate: 10
+}
+
+// Temporarily placing it here. Should replace it with the one from react-notion-x
+const defaultMapPageUrl = (rootPageId?: string) => (pageId: string) => {
+  pageId = (pageId || '').replace(/-/g, '')
+
+  if (rootPageId && pageId === rootPageId) {
+    return '/'
+  } else {
+    return `/${pageId}`
+  }
+}
+
+export async function generateStaticParams() {
+  if (isDev) {
+    return []
+  }
+
+  const mapPageUrl = defaultMapPageUrl(rootNotionPageId)
+
+  // This crawls all public pages starting from the given root page in order
+  // for next.js to pre-generate all pages via static site generation (SSG).
+  // This is a useful optimization but not necessary; you could just as easily
+  // set paths to an empty array to not pre-generate any pages at build time.
+
+  const pages = await getAllPagesInSpace(
+    rootNotionPageId,
+    rootNotionSpaceId,
+    notion.getPage,
+    {
+      traverseCollections: false
+    }
+  )
+
+  const paths = Object.keys(pages)
+    .map((pageId) => mapPageUrl(pageId))
+    .filter((path) => path && path !== '/')
+
+  return paths.map((path) => ({
+    pageId: path
+  }))
 }
 
 export default async function Page({ params }: { params: { pageId: string } }) {
